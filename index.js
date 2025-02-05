cdd4ef28368a732d42fa984542275875c9d047b1
akdoingse12 <akdoingse12@gmail.com>
2025-02-05 15:44:49 +0000

    im not done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const express = require('express')
+const app = express()
 
+const Places = [
+TopSpotsNearYou = [
+    {
+        name: "Joe's Pizza",
+        description: "Absolutely the best slice in NYC! Crispy crust and perfect cheese-to-sauce ratio.",
+        ratinganddistance: "⭐ 4.8 | 0.5 miles away",
+        location: "https://maps.google.com?q=Joe's+Pizza"
+    },
+    {
+        name: ,
+        description: ,
+        ratinganddistance: ,
+        location: 
+    }
+] 
+ ]
